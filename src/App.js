023cb6e4630aed0bea3744e24bfd6dc39adca1b1
ebluke <ethanluke.social@gmail.com>
2023-08-21 22:21:01 +1000

    publishing branch - reactJS Skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+const handleClick = () => {
+  console.log("Button work");
+};
+
+function App() {
+  return (
+    <div>
+      <button onClick={() => handleClick()}>Testing text</button>
+      <text></text>
+    </div>
+  );
+}
+
+export default App;
