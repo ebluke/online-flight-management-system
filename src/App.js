4ccead4fc8e38151e72772995adf1992b6a4e640
ebluke <ethanluke.social@gmail.com>
2023-09-18 00:10:37 +1000

    serveless connected to fe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Input, Stack, Button } from "@chakra-ui/react";
+import { UserStore } from "./stores/UserStore";
 
 function App() {
+  const userStore = UserStore;
+
   return (
     <ChakraProvider>
-      <text> Testing chackra integration</text>
+      <Stack>
+        <Input
+          type="text"
+          value={userStore.email}
+          name="email"
+          placeholder="Email"
+          size="md"
+          variant="filled"
+          onChange={(e) => userStore.set(e.target.name, e.target.value)}
+        />
+        <Input
+          value={userStore.fullName}
+          name="fullName"
+          placeholder="Full Name"
+          size="md"
+          variant="filled"
+          onChange={(e) => userStore.set(e.target.name, e.target.value)}
+        />
+        <Input
+          type="password"
+          value={userStore.password}
+          name="password"
+          placeholder="Password"
+          size="md"
+          variant="filled"
+          onChange={(e) => userStore.set(e.target.name, e.target.value)}
+        />
+        {/* Back End testing button */}
+        <Button onClick={() => userStore.createUser()}>Submit!</Button>
+      </Stack>
       {/* <Router>
         <AppWrapper>
           <Switch>
