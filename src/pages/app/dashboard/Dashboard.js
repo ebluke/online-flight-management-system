6f1df39e660bb56355ba383a4732ccfeb2600a65
E. B. Luke <72128292+ebluke@users.noreply.github.com>
2023-09-29 22:19:06 +1000

    random generation for flights complete, FutureFLights finished for now (#7)

--- src/pages/app/dashboard/Dashboard.js
+++ src/pages/app/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Box, Text, Stack, SimpleGrid } from "@chakra-ui/react";
+import { Flex, Box, Text, Stack, Icon } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 import AppLayout from "../../../components/layout/AppLayout";
@@ -9,23 +9,22 @@ import MyFlights from "./flights/MyFlights";
 import FutureFlights from "./flights/FutureFlights";
 import Panel from "../../../components/Panel";
 import CenterWrapper from "../../../components/layout/CenterWrapper";
+import { GiAirplaneDeparture } from "react-icons/gi";
 
 export default function Dashboard() {
   const navigate = useNavigate();
 
   return (
-    <Stack bgColor="brand.mantle">
-      <NavBar />
-      <AppLayout>
-        <Flex justifyContent="space-evenly">
-          <Panel onClick={() => navigate("/my-flights")}>
-            <MyFlights />
-          </Panel>
-          <Panel onClick={() => navigate("/future-flights")}>
-            <FutureFlights />
-          </Panel>
-        </Flex>
-      </AppLayout>
-    </Stack>
+    <AppLayout pageName="Dashboard">
+      <Flex justifyContent="space-evenly">
+        <Panel onClick={() => navigate("/my-flights")}>
+          <MyFlights />
+        </Panel>
+        <Panel onClick={() => navigate("/future-flights")}>
+          <Text>Future Flights</Text>
+          <Icon boxSize={16} as={GiAirplaneDeparture} />
+        </Panel>
+      </Flex>
+    </AppLayout>
   );
 }
--- src/pages/app/dashboard/flights/FutureFlights.js
+++ src/pages/app/dashboard/flights/FutureFlights.js
@@ -1,6 +1,55 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tbody,
+  Tfoot,
+  Box,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import AppLayout from "../../../../components/layout/AppLayout";
+import { FlightStore } from "../../../../stores/FlightStore";
 
 export default function FutureFlights() {
-  return <Flex>FutureFlights</Flex>;
+  //const flightUrl = "/fligh-information/";
+  // `${flightUrl}+${obj.flightNum}`
+  const flightStore = FlightStore;
+  // window.onload = function () {
+  //   flightStore.fillFlights();
+  // };
+
+  const navigate = useNavigate();
+
+  return (
+    <AppLayout pageName="Future Flights">
+      <TableContainer overflowY="scroll">
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Flight Name</Th>
+              <Th>From</Th>
+              <Th>To </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {/* generate Flights to fill data */}
+            {flightStore.flights.map((obj) => (
+              <Tr onClick={() => navigate("/flight-information")}>
+                <Td>{obj.flightNumber}</Td>
+                <Td> {obj.origin}</Td>
+                <Td> {obj.destination}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </AppLayout>
+  );
 }
